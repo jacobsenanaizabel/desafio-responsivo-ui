91affbb87d8163dfbb02834df1d5838567e3820f
jacobsenanaizabel <ana.izabel@vtex.com.br>
2017-07-21 23:53:27 -0300

    index and header.html

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,4 +1,6 @@
 app.controller('MainController', function($scope) {
 
+  $scope.title = "Desafio Web API";
+  $scope.footer = "Feito com ❤  por Ana Izabel Jacobsen :)";
 
 });
---
+++ app/services/main.service.js
@@ -0,0 +1,4 @@
+app.service('MainService', function() {
+
+
+});
