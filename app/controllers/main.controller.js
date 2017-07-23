e41dba48adce05134b983aeaf47154a36d9eeae4
jacobsenanaizabel <ana.izabel@vtex.com.br>
2017-07-23 18:46:15 -0300

    module announcement-list

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,6 +1,5 @@
 app.controller('MainController', function($scope) {
 
-  $scope.title = "Desafio Web API";
   $scope.footer = "Feito com ❤  por Ana Izabel Jacobsen :)";
 
 });
---
+++ app/modules/announcement-list/scripts/announcement.controller.js
@@ -0,0 +1,3 @@
+app.controller('AnnouncementController', function($scope) {
+
+});
